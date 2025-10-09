39d05669572b14e1e9f5de94e6aa52603dbbd352
andret0701 <andret0701@hotmail.com>
2025-10-09 13:20:18 +0200

    Moved rasterizer folder into a renderer folder

--- filelist.f
+++ filelist.f
@@ -2,12 +2,12 @@
 src/math_pkg.sv
 src/color_pkg.sv
 src/vertex_pkg.sv
-src/rasterizer/rasterizer_pkg.sv
+src/renderer/rasterizer/rasterizer_pkg.sv
 
-src/rasterizer/triangle_setup.sv
-src/rasterizer/pixel_traversal.sv
-src/rasterizer/pixel_eval.sv
-src/rasterizer/rasterizer.sv
+src/renderer/rasterizer/triangle_setup.sv
+src/renderer/rasterizer/pixel_traversal.sv
+src/renderer/rasterizer/pixel_eval.sv
+src/renderer/rasterizer/rasterizer.sv
 
 src/double_framebuffer.sv
 
